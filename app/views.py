c0d3dd3e82fff728ba49ec6e74a4f380f887c96e
Brian Hornbrook <brian_hornbrook@outlook.com>
2025-10-18 15:00:52 -0500

    added main page

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def repos(request):
+    return render(request, 'repos.html')
