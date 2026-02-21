e00002d55983e7a395a615a7f0e19ded68ca67f3
Brian Hornbrook <brian_hornbrook@outlook.com>
2026-02-21 17:39:16 -0600

    added http headers

--- brian_hornbrook_portfolio/brian_hornbrook_portfolio/settings.py
+++ brian_hornbrook_portfolio/brian_hornbrook_portfolio/settings.py
@@ -50,6 +50,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'brian_hornbrook_portfolio.urls'
 
+# Allow SSL Header with HTTPS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
