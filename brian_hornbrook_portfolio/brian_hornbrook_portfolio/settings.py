6a0d9d3dd8e263a64a4f5230bd6e16564f61c806
Brian Hornbrook <brian_hornbrook@outlook.com>
2026-02-21 17:59:24 -0600

    added whitestorage

--- brian_hornbrook_portfolio/brian_hornbrook_portfolio/settings.py
+++ brian_hornbrook_portfolio/brian_hornbrook_portfolio/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'brian_hornbrook_portfolio.urls'
@@ -118,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
