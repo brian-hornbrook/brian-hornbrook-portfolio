eb8d0315eaa0a8e424626663bd00a06085266e47
Brian Hornbrook <brian_hornbrook@outlook.com>
2026-02-21 18:42:44 -0600

    added staticfiles for static

--- brian_hornbrook_portfolio/brian_hornbrook_portfolio/settings.py
+++ brian_hornbrook_portfolio/brian_hornbrook_portfolio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG")
+DEBUG = int(os.getenv("DEBUG"))
 ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS"), "localhost"]
 
 
--- brian_hornbrook_portfolio/entrypoint.prd.sh
+++ brian_hornbrook_portfolio/entrypoint.prd.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+python manage.py collectstatic
 python -m gunicorn --bind 0.0.0.0:$PORT --workers 3 $APP_NAME.wsgi:application
